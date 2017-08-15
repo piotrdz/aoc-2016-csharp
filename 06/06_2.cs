390fa59fbef69e7fa7eeb7271c5242814984c89e
Piotr Dziwinski <piotrdz@gmail.com>
2017-08-15 15:28:09 +1200

    06.2

---
+++ 06/06_2.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Text;
+using System;
+
+using FrequencyMap = System.Collections.Generic.Dictionary<char, int>;
+
+public class AOC06_2
+{
+    static List<string> ReadInputLines()
+    {
+        var lines = new List<string>();
+
+        string line;
+        while (!String.IsNullOrEmpty(line = Console.ReadLine()))
+        {
+            lines.Add(line);            
+        }
+
+        return lines;
+    }
+
+    static string DecodeMessage(List<string> lines)
+    {
+        string decodedMessage = "";
+
+        var frequencyMapForPosition = new Dictionary<int, FrequencyMap>();
+        foreach (string line in lines)
+        {
+            foreach (var it in line.Select((x,i) => new { Value = x, Index = i }))
+            {
+                if (!frequencyMapForPosition.ContainsKey(it.Index))
+                {
+                    frequencyMapForPosition[it.Index] = new FrequencyMap();
+                }
+
+                var positionFreqMap = frequencyMapForPosition[it.Index];
+                if (!positionFreqMap.ContainsKey(it.Value))
+                {
+                    positionFreqMap[it.Value] = 1;
+                }
+                else
+                {
+                    ++positionFreqMap[it.Value];
+                }
+            }
+        }
+
+        int maxPosition = frequencyMapForPosition.Keys.Max();
+        for (int position = 0; position <= maxPosition; ++position)
+        {
+            var positionFreqMap = frequencyMapForPosition[position];
+            int maxFreqCount = positionFreqMap.Values.Min();
+            char mostCommonCh = positionFreqMap.FirstOrDefault(x => x.Value == maxFreqCount).Key;
+            decodedMessage += mostCommonCh;
+        }
+
+        return decodedMessage;
+    }
+
+    public static void Main()
+    {
+        List<string> lines = ReadInputLines();
+
+        string decodedMessage = DecodeMessage(lines);
+        Console.WriteLine(decodedMessage);
+    }
+}
